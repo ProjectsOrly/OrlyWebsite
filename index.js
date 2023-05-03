35660e8e146d0baca29d9e414468b728d8d5363e
yovelovadia <yovel78910@gmail.com>
2023-05-03 21:06:44 +0300

    New version

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import json from "./version.json" assert { type: "json" };
+import json from "./versionV1.2.0.json" assert { type: "json" };
 
 document.getElementById("version").innerHTML = json.version;
