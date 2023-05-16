43603327dacca04a2c22ec7951d0ca5f2dfeb151
yovelovadia <yovel78910@gmail.com>
2023-05-16 17:41:58 +0300

    Add Q&A

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import json from "./version.json" assert { type: "json" };
 
 document.getElementById("version").innerHTML = json.version;
+
+
+const questionBoxes = document.querySelectorAll('.questionBox');
+
+questionBoxes.forEach(questionBox => {
+  const answer = questionBox.querySelector('p');
+
+  questionBox.addEventListener('click', () => {
+    questionBoxes.forEach(box => {
+      if (box !== questionBox) {
+        box.querySelector('p').classList.remove('active');
+      }
+    });
+    answer.classList.toggle('active');
+  });
+});
+
+
