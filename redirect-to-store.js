311196601c1173164a11030bd4ca5d55c756b1c3
yovelovadia <yovel78910@gmail.com>
2023-05-20 10:56:35 +0300

    redirect support mac/windows && Start of terms

--- redirect-to-store.js
+++ redirect-to-store.js
@@ -8,6 +8,14 @@ if (/android/i.test(userAgent)) {
 else if (/iPhone/.test(userAgent) || userAgent.indexOf('iPad') !== -1 || userAgent.indexOf('iPod') !== -1) {
   window.location.href = 'https://apps.apple.com/us/app/orly/id6446028123';
 }
+// Check if the user agent indicates a Windows device
+else if (/Windows/i.test(userAgent)) {
+  window.location.href = 'https://play.google.com/store/apps/details?id=com.orlyapp';
+}
+// Check if the user agent indicates a Mac device
+else if (/Mac/i.test(userAgent)) {
+  window.location.href = 'https://apps.apple.com/us/app/orly/id6446028123';
+}
 // Default redirection in case the device type is unknown or unsupported
 else {
   // Redirect to a fallback URL or display an error message
